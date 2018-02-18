1897a041d7d2831a62f561f760efad34d629ce96
Avinash Bhardwaj <avinashb97@gmail.com>
2018-02-18 05:59:58 +0530

    Formatted form details, setup databse, initialised angular app

--- app.js
+++ app.js
@@ -2,30 +2,25 @@ const express = require('express');
 const app = express();
 const nodemailer = require('nodemailer');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const connectionURL = 'mongodb://avinashb98:openddoor@ds141028.mlab.com:41028/contact-form'
+mongoose.connect(connectionURL, () => {
+  console.log('Connected to database...');
+});
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const transporter = nodemailer.createTransport({
  service: 'gmail',
  auth: {
-        user: 'avitest98.com',
+        user: 'avitest98@gmail.com',
         pass: 'openddoor'
     }
 });
-
-const mailOptions = {
-  from: 'avitest98@gmail.com', // sender address
-  to: 'avinashb97@gmail.com', // list of receivers
-  subject: 'Nodemailer Test', // Subject line
-  html: '<h1>Yaay!! Success</h1>'// plain text body
-};
-
-// transporter.sendMail(mailOptions, function (err, info) {
-//    if(err)
-//      console.log(err)
-//    else
-//      console.log(info);
-// });
-
-app.use(cors());
 
 app.get('/', (req, res) => {
   res.send({
@@ -33,7 +28,32 @@ app.get('/', (req, res) => {
   });
 });
 
+app.post('/', (req, res) => {
+  let mailHTML = `<h3>${req.body.name} submitted the contact us form and here are the details.</h3>`;
+  for(key in req.body) {
+    mailHTML += `<br><p>${key}: ${req.body[key]}</p>`
+  }
+  const mailOptions = {
+    from: 'avitest98@gmail.com',
+    to: 'avinashb97@gmail.com',
+    subject: 'Nodemailer Test',
+    html: mailHTML
+  };
 
+  transporter.sendMail(mailOptions, function (err, info) {
+     if(err) {
+       console.log(err);
+       res.status(400).send({
+         message: 'error occurred'
+       });
+     }
+     else {
+       res.status(200).send({
+         message: 'Success'
+       });
+     }
+  });
+});
 
 app.listen(3000, () => {
   console.log('Server running on port 3000...');
